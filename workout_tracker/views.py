a1e83936f446d8d7e43021d0587f6d5c426f36a1
Seif Mohamed <seifkeshk4@gmail.com>
2015-03-15 16:22:09 +0200

    trainer profile updates #11

--- workout_tracker/views.py
+++ workout_tracker/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+def view_trainerprofile(request):
+	user = {
+	  "name":"seif",
+	  "email":"seif@gmail.com"
+	 }
+
+	return render(request, 'trainer_profile.html', {"user":user})
 # Create your views here.
 
