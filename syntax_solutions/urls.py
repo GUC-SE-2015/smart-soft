a1651a86e3451f4e37a0a62720b08f56024f21ff
unknown <sarahmaharem@gmail.com>
2015-03-18 17:36:46 +0200

    i finished the login task with errors

--- syntax_solutions/urls.py
+++ syntax_solutions/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from workout_tracker import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,3 +11,12 @@ urlpatterns = patterns('',
     url(r'^friends/', include(admin.site.urls)),
     url(r'^$', 'workout_tracker.views.show'),
 )
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_url>\w+)$', views.category, name='category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    )
