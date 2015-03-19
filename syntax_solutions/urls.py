e8664f770317c692c022f49597448eead9958772
unknown <sarahmaharem@gmail.com>
2015-03-19 10:18:03 +0200

    login and logout are done with html code and urls edited as well as the index

--- syntax_solutions/urls.py
+++ syntax_solutions/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
         url(r'^my_clients/$', views.view_clients),
         url(r'^trainer_profile/$',views.view_trainerprofile),
         url(r'^search/$', views.search),
-    #url(r'^register/', views.register, name='register'),
- url(r'^login/$', views.user_login, name='login'),
-   
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),   
+    url(r'^logout/$', views.user_logout, name='logout')
 )
