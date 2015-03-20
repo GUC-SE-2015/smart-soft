4f56cffa8897c188eb4eda95672cdf6e0ed77447
Eric Philippe <eric.greiss@outlook.com>
2015-03-20 17:23:28 +0200

    missing urls

--- syntax_solutions/urls.py
+++ syntax_solutions/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^friends/', include(admin.site.urls)),
-    url(r'^$', 'workout_tracker.views.show'),
+    url(r'^home/$', home),
+    url(r'^home/search/$', search, name='search'),
+  
 )
 
